044319364a136c9c3296ae6d262b074afb53cfd9
RIssa <rissa_cford@yahoo.com>
2020-04-05 01:07:39 -0700

    making sure I had my work saved when i pushed

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useFindAndModify: false
 });
 
-// routes
 app.use(require("./routes/api.js"));
 app.use(require("./routes.view.js"));
 
